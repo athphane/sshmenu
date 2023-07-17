9260d87176567f226dbc05e0fdb64867ded4a1f5
athphane <athphane@gmail.com>
2023-07-18 00:22:03 +0500

    bring back argparse so i can add new hosts

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import argparse
 import json
 import os
+import time
 
 from colorama import init as colorama_init
 from simple_term_menu import TerminalMenu
 
 from helpers.connection import run_ssh_command
-from helpers.ip_address import obfuscate_ip, is_ip_address
+from helpers.host_modification import add_host, remove_host
 
 colorama_init()
 
@@ -24,39 +26,40 @@ def main():
     with open(os.environ['PATH_TO_HOSTS']) as f:
         data = json.load(f)["hosts"]
 
-    options = []
+    parser = argparse.ArgumentParser(description='SSH Host Management')
+    parser.add_argument('--add', action='store_true', help='Add a new host')
+    parser.add_argument('--remove', action='store_true', help='Remove an existing host')
 
-    for index, record in enumerate(data, start=1):
-        if 'hide_address' in record.keys() and record['hide_address'] is True:
-            obfuscated_address = '*' * len(record['address'])
-        else:
-            obfuscated_address = obfuscate_ip(record['address']) if is_ip_address(record['address']) else record[
-                'address']
+    args = parser.parse_args()
 
-        values = record['name']
-        options.append(values)
+    if args.add:
+        add_host(data)
+    elif args.remove:
+        remove_host(data)
+    else:
+        options = [record['name'] for record in data]
 
-    main_menu_title = "  Main Menu.\n  Press Q or Esc to quit. \n"
-    main_menu_cursor = "> "
-    main_menu_cursor_style = ("fg_blue", "bold")
-    main_menu_style = ("bg_blue", "fg_black")
-    menu_highlight_style = ("bg_blue", "fg_black")
-    terminal_menu = TerminalMenu(
-        options,
-        title=main_menu_title,
-        menu_cursor=main_menu_cursor,
-        menu_cursor_style=main_menu_cursor_style,
-        menu_highlight_style=main_menu_style,
-        search_highlight_style=menu_highlight_style,
-        cycle_cursor=True,
-        clear_screen=True,
-    )
-    menu_entry_index = terminal_menu.show()
+        main_menu_title = "  Main Menu.\n  Press Q or Esc to quit. \n"
+        main_menu_cursor = "> "
+        main_menu_cursor_style = ("fg_blue", "bold")
+        main_menu_style = ("bg_blue", "fg_black")
+        menu_highlight_style = ("bg_blue", "fg_black")
+        terminal_menu = TerminalMenu(
+            options,
+            title=main_menu_title,
+            menu_cursor=main_menu_cursor,
+            menu_cursor_style=main_menu_cursor_style,
+            menu_highlight_style=main_menu_style,
+            search_highlight_style=menu_highlight_style,
+            cycle_cursor=True,
+            clear_screen=True,
+        )
+        menu_entry_index = terminal_menu.show()
 
-    print(f"Connecting to {data[menu_entry_index]}")
+        print(f"Connecting to {data[menu_entry_index]}")
+        time.sleep(0.5)
 
-    run_ssh_command(data[menu_entry_index])
-    # print(f"You have selected {options[menu_entry_index]}!")
+        run_ssh_command(data[menu_entry_index])
 
 
 if __name__ == '__main__':
